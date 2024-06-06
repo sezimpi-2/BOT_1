8c3cc170901dde36bd65555d9374c1436406920d
sezimpi-2 <cezim.tabaldyeva@gmail.com>
2024-06-06 18:02:48 +0600

    д/з 2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 from dotenv import load_dotenv
 from aiogram.types import FSInputFile
 from os import getenv
